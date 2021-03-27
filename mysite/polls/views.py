d3fc3ce2e8dbdabe45321da5586b030f57a5c376
sadeq hashemi <sadeqmohammadh@gmail.com>
2021-03-27 20:47:41 +0430

    rewrite index with render shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
@@ -14,11 +14,10 @@ def results(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def vote(request, question_id):
