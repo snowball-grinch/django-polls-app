0816d8e3eb39217fe412f67242631d54292bb9a5
sadeq hashemi <sadeqmohammadh@gmail.com>
2021-03-28 20:39:37 +0430

    rewrite details with get_object_or_404 instead of try and except block

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', context={'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
